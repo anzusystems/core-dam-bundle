a38d093464cebd270375c52c429a309238cee4bb
Ronald Marfoldi <marforon@gmail.com>
2023-02-14 16:23:52 +0100

    Fix typehint

--- src/Repository/VideoFileRepository.php
+++ src/Repository/VideoFileRepository.php
@@ -4,16 +4,15 @@ declare(strict_types=1);
 
 namespace AnzuSystems\CoreDamBundle\Repository;
 
-use AnzuSystems\CoreDamBundle\Entity\DocumentFile;
 use AnzuSystems\CoreDamBundle\Entity\VideoFile;
 
 /**
- * @extends AbstractAnzuRepository<DocumentFile>
+ * @extends AbstractAnzuRepository<VideoFile>
  *
- * @method DocumentFile|null find($id, $lockMode = null, $lockVersion = null)
- * @method DocumentFile|null findOneBy($id, $lockMode = null, $lockVersion = null)
- * @method DocumentFile|null findProcessedById(string $id)
- * @method DocumentFile|null findProcessedByIdAndFilename(string $id, string $slug)
+ * @method VideoFile|null find($id, $lockMode = null, $lockVersion = null)
+ * @method VideoFile|null findOneBy($id, $lockMode = null, $lockVersion = null)
+ * @method VideoFile|null findProcessedById(string $id)
+ * @method VideoFile|null findProcessedByIdAndFilename(string $id, string $slug)
  */
 final class VideoFileRepository extends AbstractAssetFileRepository
 {
