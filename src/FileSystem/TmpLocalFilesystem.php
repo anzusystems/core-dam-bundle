ba8b3a7ad15af76eae70dc7d63a9579dcb560fb0
Tomas Hermanek <tomhermanek@gmail.com>
2023-03-27 16:31:03 +0200

    feature AB#72151 Image processor. Dont create default roi if exists.

--- src/FileSystem/TmpLocalFilesystem.php
+++ src/FileSystem/TmpLocalFilesystem.php
@@ -18,6 +18,18 @@ final class TmpLocalFilesystem extends LocalFilesystem
     private array $paths = [];
     private NameGenerator $nameGenerator;
 
+    /**
+     * @throws FilesystemException
+     */
+    public function clearPaths(): void
+    {
+        foreach ($this->paths as $path) {
+            if ($this->fileExists($path)) {
+                $this->delete($path);
+            }
+        }
+    }
+
     public function getTmpFileName(?string $extension = null): string
     {
         $path = $this->nameGenerator->generatePath($extension)->getFileName();
