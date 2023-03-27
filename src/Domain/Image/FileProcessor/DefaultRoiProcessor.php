850a07434452bb1b94c33e4b4e6b90ac4c6884ae
Tomas Hermanek <tomhermanek@gmail.com>
2023-03-27 08:17:30 +0200

    feature AB#72151 Image processor. Dont create default roi if exists.

--- src/Domain/Image/FileProcessor/DefaultRoiProcessor.php
+++ src/Domain/Image/FileProcessor/DefaultRoiProcessor.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 
 namespace AnzuSystems\CoreDamBundle\Domain\Image\FileProcessor;
 
+use AnzuSystems\CoreDamBundle\App;
 use AnzuSystems\CoreDamBundle\Domain\RegionOfInterest\DefaultRegionOfInterestFactory;
 use AnzuSystems\CoreDamBundle\Domain\RegionOfInterest\RegionOfInterestManager;
 use AnzuSystems\CoreDamBundle\Entity\AssetFile;
 use AnzuSystems\CoreDamBundle\Entity\ImageFile;
+use AnzuSystems\CoreDamBundle\Entity\RegionOfInterest;
 use AnzuSystems\CoreDamBundle\Model\Dto\File\AdapterFile;
 
-final class DefaultRoiProcessor
+final readonly class DefaultRoiProcessor
 {
     public function __construct(
-        private readonly DefaultRegionOfInterestFactory $defaultRegionOfInterestFactory,
-        private readonly RegionOfInterestManager $regionOfInterestManager,
+        private DefaultRegionOfInterestFactory $defaultRegionOfInterestFactory,
+        private RegionOfInterestManager $regionOfInterestManager,
     ) {
     }
 
@@ -23,6 +25,14 @@ final class DefaultRoiProcessor
      */
     public function process(AssetFile $assetFile, AdapterFile $file): AssetFile
     {
+        $defaultRoi = $assetFile->getRegionsOfInterest()->filter(
+            fn (RegionOfInterest $roi): bool => App::ZERO === $roi->getPosition()
+        )->first();
+
+        if ($defaultRoi instanceof RegionOfInterest) {
+            return $assetFile;
+        }
+
         $roi = $this->defaultRegionOfInterestFactory->prepareDefaultRoi($assetFile);
         $assetFile->getRegionsOfInterest()->add($roi);
         $roi->setImage($assetFile);
