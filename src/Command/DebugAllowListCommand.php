2e4b24bacd94d2fdc521a8473c7b100ecb4d5a46
Tomas Hermanek <tomhermanek@gmail.com>
2023-04-13 17:40:28 +0200

    Allow list debug command.

--- src/Command/DebugAllowListCommand.php
+++ src/Command/DebugAllowListCommand.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace AnzuSystems\CoreDamBundle\Command;
 
 use AnzuSystems\CoreDamBundle\Command\Traits\OutputUtilTrait;
+use AnzuSystems\CoreDamBundle\Model\Configuration\CacheConfiguration;
+use AnzuSystems\CoreDamBundle\Model\Configuration\CropAllowListConfiguration;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
@@ -18,12 +20,14 @@ use Symfony\Component\Console\Output\OutputInterface;
 )]
 final class DebugAllowListCommand extends Command
 {
-    private const ARG_EXT_SYSTEM_SLUG = 'ext_system_slug';
-
     use OutputUtilTrait;
+    private const ARG_EXT_SYSTEM_SLUG = 'ext_system_slug';
 
     public function __construct(
         private readonly array $domainAllowMap,
+        private readonly array $domains,
+        private readonly array $domainAllowList,
+        private readonly array $domainNames,
     ) {
         parent::__construct();
     }
@@ -38,25 +42,61 @@ final class DebugAllowListCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $table  = (new Table($output))
-            ->setHeaders(['key', 'crop_allow_list', 'ext_system_slugs', 'domain']);
+        $extSystemSlug = (string) $input->getArgument(self::ARG_EXT_SYSTEM_SLUG);
+        $table = (new Table($output))
+            ->setHeaders(['domain', 'crop_allow_list', 'width', 'height', 'title', 'tags']);
+
+        /** @var array<string, CacheConfiguration> $domainsToDebug * */
+        $domainsToDebug = [];
 
         /**
-         * @var string $key
          * @var array{
          *     crop_allow_list: string,
          *     domain: string,
          *     ext_system_slugs: array<int, string>
          * } $domainAllowMap
          */
-        foreach ($this->domainAllowMap as $key => $domainAllowMap) {
-            $table->addRow([
-                $key, $domainAllowMap['crop_allow_list'], implode(', ', $domainAllowMap['ext_system_slugs']),  $domainAllowMap['domain']
-            ]);
+        foreach ($this->domainAllowMap as $domainAllowMap) {
+            if (false === in_array($extSystemSlug, $domainAllowMap['ext_system_slugs'], true)) {
+                continue;
+            }
+
+            $domainConfig = $this->getDomain($domainAllowMap['domain']);
+            $domainsToDebug[$domainAllowMap['crop_allow_list']] = $domainConfig;
+        }
+
+        foreach ($domainsToDebug as $allowList => $domain) {
+            if (false === isset($this->domainAllowList[$allowList])) {
+                continue;
+            }
+
+            $cropConfig = CropAllowListConfiguration::getFromArrayConfiguration($this->domainAllowList[$allowList]);
+
+            foreach ($cropConfig->getCrops() as $crop) {
+                $table->addRow(
+                    [
+                        $domain->getDomain(),
+                        $allowList,
+                        $crop['width'],
+                        $crop['height'],
+                        $crop['title'],
+                        implode(', ', $crop['tags']),
+                    ]
+                );
+            }
         }
 
         $table->render();
 
         return Command::SUCCESS;
+    }
+
+    private function getDomain(string $name): CacheConfiguration
+    {
+        if (isset($this->domains[$name])) {
+            return CacheConfiguration::getFromArrayConfiguration($this->domains[$name]);
+        }
+
+        return new CacheConfiguration();
     }
 }
