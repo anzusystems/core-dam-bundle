e0a3ca9fc8e78810ea815ea6998c19110cf33182
Tomas Hermanek <tomhermanek@gmail.com>
2024-01-03 14:32:07 +0100

    package updates, fixed tests.

--- tests/config/routing/routing.php
+++ tests/config/routing/routing.php
@@ -6,15 +6,15 @@ namespace Symfony\Component\Routing\Loader\Configurator;
 
 return static function (RoutingConfigurator $routes): void {
     $routes
-        ->import('@AnzuSystemsCoreDamBundle/Controller/Api/Adm/V1', type: 'annotation')
+        ->import('@AnzuSystemsCoreDamBundle/Controller/Api/Adm/V1', type: 'attribute')
         ->prefix('/api/adm/v1/');
 
     $routes
-        ->import('@AnzuSystemsCoreDamBundle/Controller/Api/Pub/V1', type: 'annotation')
+        ->import('@AnzuSystemsCoreDamBundle/Controller/Api/Pub/V1', type: 'attribute')
         ->prefix('/api/pub/v1/');
 
     $routes
-        ->import('@AnzuSystemsCoreDamBundle/Controller/ImageController.php', type: 'annotation')
+        ->import('@AnzuSystemsCoreDamBundle/Controller/ImageController.php', type: 'attribute')
         ->prefix('/');
 
     $routes
