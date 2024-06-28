4ba486a3911c7c6061da115fd14ce02267dff54a
Tomas Hermanek <tomhermanek@gmail.com>
2024-06-28 08:22:40 +0200

    psalm fix

--- src/Domain/Distribution/DistributionManagerProvider.php
+++ src/Domain/Distribution/DistributionManagerProvider.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace AnzuSystems\CoreDamBundle\Domain\Distribution;
 
-use Symfony\Component\DependencyInjection\Attribute\TaggedLocator;
+use Symfony\Component\DependencyInjection\Attribute\AutowireLocator;
 use Symfony\Component\DependencyInjection\ServiceLocator;
 
 final readonly class DistributionManagerProvider
@@ -12,7 +12,7 @@ final readonly class DistributionManagerProvider
     private ServiceLocator $manager;
 
     public function __construct(
-        #[TaggedLocator(DistributionManagerInterface::class, indexAttribute: 'key')]
+        #[AutowireLocator(DistributionManagerInterface::class, indexAttribute: 'key')]
         ServiceLocator $manager,
     ) {
         $this->manager = $manager;
