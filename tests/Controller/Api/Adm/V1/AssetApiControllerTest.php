38349724a4021a8873a12e34a1a6f32969e9294e
TomasHermanek <tomhermanek@gmail.com>
2024-12-20 11:46:26 +0100

    API for bulk metadata update. Now does not clean undefined fields. (#99)
    * API for bulk metadata update. Now does not clean undefined fields.

--- tests/Controller/Api/Adm/V1/AssetApiControllerTest.php
+++ tests/Controller/Api/Adm/V1/AssetApiControllerTest.php
@@ -6,6 +6,8 @@ declare(strict_types=1);
 namespace AnzuSystems\CoreDamBundle\Tests\Controller\Api\Adm\V1;
 
 use AnzuSystems\CoreDamBundle\DataFixtures\AssetLicenceFixtures;
+use AnzuSystems\CoreDamBundle\DataFixtures\AuthorFixtures;
+use AnzuSystems\CoreDamBundle\DataFixtures\KeywordFixtures;
 use AnzuSystems\CoreDamBundle\DataFixtures\VideoFixtures;
 use AnzuSystems\CoreDamBundle\Domain\Image\ImageUrlFactory;
 use AnzuSystems\CoreDamBundle\Entity\Asset;
@@ -14,10 +16,13 @@ use AnzuSystems\CoreDamBundle\Entity\VideoFile;
 use AnzuSystems\CoreDamBundle\Tests\Controller\Api\AbstractAssetFileApiController;
 use AnzuSystems\CoreDamBundle\Tests\Data\Entity\User;
 use AnzuSystems\CoreDamBundle\Tests\Data\Fixtures\DistributionCategoryFixtures;
+use AnzuSystems\CoreDamBundle\DataFixtures\ImageFixtures;
 use AnzuSystems\CoreDamBundle\Tests\Data\Model\AssetUrl\ImageUrl;
 use AnzuSystems\SerializerBundle\Exception\SerializerException;
 use League\Flysystem\FilesystemException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Uid\Uuid;
+use Symfony\Component\Uid\UuidV4;
 
 final class AssetApiControllerTest extends AbstractAssetFileApiController
 {
@@ -65,6 +70,149 @@ final class AssetApiControllerTest extends AbstractAssetFileApiController
                 DistributionCategoryFixtures::CATEGORY_4,
             ],
         ];
+    }
+
+    public function testMetadataBulkUpdate(): void
+    {
+        $client = $this->getApiClient(User::ID_ADMIN);
+
+        $firstImage = $this->entityManager->getRepository(ImageFile::class)->find(ImageFixtures::IMAGE_ID_1_1);
+        $firstAsset = $firstImage->getAsset();
+
+        $response = $client->patch(
+            '/api/adm/v1/asset/metadata-bulk-update',
+            [
+                [
+                    'id' => (new UuidV4())->toRfc4122(),
+                ]
+            ]
+        );
+        $this->assertSame(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+
+        $response = $client->patch(
+            '/api/adm/v1/asset/metadata-bulk-update',
+            [
+                [
+                    'id' => $firstAsset->getId(),
+                ]
+            ]
+        );
+
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+
+        $firstAsset = $this->entityManager->getRepository(ImageFile::class)->find(ImageFixtures::IMAGE_ID_1_1)->getAsset();
+        $this->assertSame($firstAsset->getMetadata()->getCustomData(), [
+            'title' => 'Custom Data Title',
+            'headline' => 'Custom Data Headline',
+            'description' => 'Custom Data Description',
+        ]);
+        $this->assertSame($firstAsset->getAssetFlags()->isDescribed(), true);
+        $this->assertSame($firstAsset->getMainFile()->getFlags()->isSingleUse(), true);
+        $this->assertSame(
+            $firstAsset->getAuthors()->map(fn($author) => $author->getName())->getValues(),
+            ["Larry Queen"]
+        );
+        $this->assertSame(
+            $firstAsset->getKeywords()->map(fn($author) => $author->getName())->getValues(),
+            ["Politics"]
+        );
+
+        $response = $client->patch(
+            '/api/adm/v1/asset/metadata-bulk-update',
+            [
+                [
+                    'id' => $firstAsset->getId(),
+                    'described' => false,
+                    'mainFileSingleUse' => false,
+                    'customData' => [],
+                    'keywords' => [],
+                    'authors' => [],
+                ]
+            ]
+        );
+
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+
+        $firstAsset = $this->entityManager->getRepository(ImageFile::class)->find(ImageFixtures::IMAGE_ID_1_1)->getAsset();
+        $this->assertSame($firstAsset->getMetadata()->getCustomData(), []);
+        $this->assertSame($firstAsset->getAssetFlags()->isDescribed(), false);
+        $this->assertSame($firstAsset->getMainFile()->getFlags()->isSingleUse(), false);
+        $this->assertCount(0, $firstAsset->getAuthors());
+        $this->assertCount(0, $firstAsset->getKeywords());
+
+        $response = $client->patch(
+            '/api/adm/v1/asset/metadata-bulk-update',
+            [
+                [
+                    'id' => $firstAsset->getId(),
+                    'described' => true,
+                    'mainFileSingleUse' => true,
+                ]
+            ]
+        );
+
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+
+        $firstAsset = $this->entityManager->getRepository(ImageFile::class)->find(ImageFixtures::IMAGE_ID_1_1)->getAsset();
+        $this->assertSame($firstAsset->getAssetFlags()->isDescribed(), true);
+        $this->assertSame($firstAsset->getMainFile()->getFlags()->isSingleUse(), true);
+
+        $response = $client->patch(
+            '/api/adm/v1/asset/metadata-bulk-update',
+            [
+                [
+                    'id' => $firstAsset->getId(),
+                    'customData' => [
+                        'title' => 'Title',
+                        'headline' => 'Headline',
+                        'description' => 'Description',
+                    ]
+                ]
+            ]
+        );
+
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+
+        $firstAsset = $this->entityManager->getRepository(ImageFile::class)->find(ImageFixtures::IMAGE_ID_1_1)->getAsset();
+        $this->assertEqualsCanonicalizing($firstAsset->getMetadata()->getCustomData(), [
+            'title' => 'Title',
+            'headline' => 'Headline',
+            'description' => 'Description',
+        ]);
+        $this->assertSame($firstAsset->getAssetFlags()->isDescribed(), true);
+        $this->assertSame($firstAsset->getMainFile()->getFlags()->isSingleUse(), true);
+
+        $response = $client->patch(
+            '/api/adm/v1/asset/metadata-bulk-update',
+            [
+                [
+                    'id' => $firstAsset->getId(),
+                    'keywords' => [
+                        KeywordFixtures::KEYWORD_2
+                    ],
+                    'authors' => [
+                        AuthorFixtures::AUTHOR_2
+                    ],
+                ]
+            ]
+        );
+
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+
+        $firstAsset = $this->entityManager->getRepository(ImageFile::class)->find(ImageFixtures::IMAGE_ID_1_1)->getAsset();
+        $this->assertEqualsCanonicalizing($firstAsset->getMetadata()->getCustomData(), [
+            'title' => 'Title',
+            'headline' => 'Headline',
+            'description' => 'Description',
+        ]);
+        $this->assertSame(
+            $firstAsset->getAuthors()->map(fn($author) => $author->getName())->getValues(),
+            ["Aarne Ormonde"]
+        );
+        $this->assertSame(
+            $firstAsset->getKeywords()->map(fn($author) => $author->getName())->getValues(),
+            ["Podcast"]
+        );
     }
 
     protected function setUp(): void
