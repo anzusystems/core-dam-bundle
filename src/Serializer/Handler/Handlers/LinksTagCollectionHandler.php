1be727bf62c4439ad5f3066446b7e6978de16377
Tomas Hermanek <tomhermanek@gmail.com>
2025-01-28 09:58:41 +0100

    Fixed.

--- src/Serializer/Handler/Handlers/LinksTagCollectionHandler.php
+++ src/Serializer/Handler/Handlers/LinksTagCollectionHandler.php
@@ -6,21 +6,10 @@ namespace AnzuSystems\CoreDamBundle\Serializer\Handler\Handlers;
 
 use AnzuSystems\CoreDamBundle\Entity\ImageFile;
 use AnzuSystems\CoreDamBundle\Model\Enum\AssetFileProcessStatus;
-use AnzuSystems\SerializerBundle\Helper\SerializerHelper;
 use AnzuSystems\SerializerBundle\Metadata\Metadata;
 
 class LinksTagCollectionHandler extends LinksHandler
 {
-    public function describe(string $property, Metadata $metadata): array
-    {
-        $description = [
-            'property' => $property,
-            'type' => SerializerHelper::getOaFriendlyType($metadata->type),
-        ];
-
-        return $description;
-    }
-
     protected function getImageFileLinks(ImageFile $imageFile, Metadata $metadata): array
     {
         if ($imageFile->getAssetAttributes()->getStatus()->isNot(AssetFileProcessStatus::Processed)) {
