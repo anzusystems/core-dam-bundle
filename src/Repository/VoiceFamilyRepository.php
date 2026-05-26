28a43e535c751af0824c3d5d1f28c2545198bf6f
Tomas Hermanek <tomas.hermanek@petitpress.sk>
2026-05-26 11:14:33 +0000

    add #84735 voice-family repo find-without-binding for cleanup cron

--- src/Repository/VoiceFamilyRepository.php
+++ src/Repository/VoiceFamilyRepository.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace AnzuSystems\CoreDamBundle\Repository;
 
 use AnzuSystems\CoreDamBundle\Entity\ExtSystem;
+use AnzuSystems\CoreDamBundle\Entity\Voice;
 use AnzuSystems\CoreDamBundle\Entity\VoiceFamily;
+use AnzuSystems\CoreDamBundle\Model\Enum\VoiceDiscriminator;
 
 /**
  * @extends AbstractAnzuRepository<VoiceFamily>
@@ -21,6 +23,22 @@ final class VoiceFamilyRepository extends AbstractAnzuRepository
             'slug' => $slug,
             'extSystem' => $extSystem,
         ]);
+    }
+
+    /**
+     * @return VoiceFamily[]
+     */
+    public function findFamiliesWithoutBindingFor(VoiceDiscriminator $discriminator): array
+    {
+        return $this->createQueryBuilder('vf')
+            ->andWhere(
+                'NOT EXISTS (SELECT 1 FROM ' . Voice::class . ' v WHERE v.voiceFamily = vf AND v INSTANCE OF :discriminator)',
+            )
+            ->setParameter('discriminator', $discriminator->value)
+            ->orderBy('vf.extSystem', 'ASC')
+            ->addOrderBy('vf.slug', 'ASC')
+            ->getQuery()
+            ->getResult();
     }
 
     protected function getEntityClass(): string
